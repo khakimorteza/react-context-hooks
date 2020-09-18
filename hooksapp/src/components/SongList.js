968515843dec414b28b7d9313714044ad7d8142c
morteza khaki <mortezakhaki@pursuit.org>
2020-09-18 02:18:22 -0400

    hooksapp

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const songList = () => {
+    return ( 
+        <div className='song-list'>
+            <ul>
+                <li>this wildly darkness</li>
+                <li>memory gospel</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default songList;
