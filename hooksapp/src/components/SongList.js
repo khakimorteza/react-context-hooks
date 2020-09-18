eb8f7c14ef96e1361f9b31cb64db2ea79b02fa8c
morteza khaki <mortezakhaki@pursuit.org>
2020-09-18 03:31:04 -0400

    useState hook added to SongList

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,14 +1,27 @@
-import React from 'react';
 
-const songList = () => {
-    return ( 
-        <div className='song-list'>
-            <ul>
-                <li>this wildly darkness</li>
-                <li>memory gospel</li>
-            </ul>
-        </div>
-     );
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'almost home', id: 1 },
+    { title: 'memory gospel', id: 2 },
+    { title: 'this wild darkness', id: 3 }
+  ]);
+//   console.log(songs);
+  const addSong = () => {
+    setSongs([...songs, { title: 'new song', id: uuidv4() }]);
+  };
+  return (
+    <div className="song-list">
+      <ul>
+        {songs.map(song => {
+          return ( <li key={song.id}>{song.title}</li> );
+        })}
+      </ul>
+      <button onClick={addSong}>Add a song</button>
+    </div>
+  );
 }
- 
-export default songList;
+
+export default SongList;
